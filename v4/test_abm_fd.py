6ec22d44215a8f5a97d0e97b38a32e46b779d32a
Bharat Kunwar <b.kunwar@gmail.com>
2016-02-08 01:00:04 +0000

    Fundamental diagram sensitivity analysis for TGF 2015 paper

--- v4/test_abm_fd.py
+++ v4/test_abm_fd.py
@@ -1,27 +1,30 @@
+# Used for the TGF 2015 paper
 import sys
 sys.path.append('core')
 import abm, db, csv, numpy as np
 reload(abm)
 reload(db)
+import pickle
 
 self = abm.Sim('test_abm_fd',(-2.88612967859241, 54.91133135582125, -2.9609706894330743, 54.88327220183169))
 self.h.destins = [2551]
 self.scenarios = ['ia']
 
-x = [50000]#[25000,50000,100000,200000]
-y = [4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3]
-z = [5.0,5.5,6.0,6.5,7.0,7.5,8.0,8.5,9.0]
-y = np.arange(start=4.4,stop=5.36,step=0.1)
-z = np.arange(start=5.0,stop=9.1,step=0.5)
-print len(x),len(y),len(z),len(x)*len(y)*len(z)
+X = [25000,50000,100000,200000]
+Y = [4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3]
+Z = [5.0,5.5,6.0,6.5,7.0,7.5,8.0,8.5,9.0]
+print len(X),len(Y),len(Z),len(X)*len(Y)*len(Z)
 
-resarray = np.zeros((len(x),len(y),len(z)))
+# ---------------
+# Write to csv cache
 
-for i,self.n in enumerate(x):
+resarray = np.zeros((len(X),len(Y),len(Z)))
+
+for i,self.n in enumerate(X):
     with open('data/tgf2015/{}.csv'.format(self.n), 'w') as fp:
         a = csv.writer(fp, delimiter=',');
-        for j,abm.fd.k_vmin in enumerate(y):
-            for k,abm.fd.k_lim in enumerate(z):
+        for j,abm.fd.k_vmin in enumerate(Y):
+            for k,abm.fd.k_lim in enumerate(Z):
                 abm.fd.precomputation()
                 self.use_buffer = False
                 self.run(agent_progress_bar=False)
@@ -30,15 +33,30 @@ for i,self.n in enumerate(x):
                 print data
                 a.writerows(data)
 
-import pickle
-with open('file.pickle','wb') as f:
+# ---------------
+# Dump/read resarray
+# However, we probably dont need to use it 
+
+with open('data/tgf2015/fd-resarray.pickle','wb') as f:
     pickle.dump(resarray,f)
 
-with open('file.pickle','rb') as f:
+with open('data/tgf2015/fd-resarray.pickle','rb') as f:
     resarray = pickle.load(f)
 
+# ---------------
+# Read from csv cache
 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
 
+# ---------------
+# Draw 3D plot
+
+plt.ion()
+plt.close('all')
 for n in [25000,50000,100000,200000]:
 
     x = []
@@ -56,15 +74,9 @@ for n in [25000,50000,100000,200000]:
 
     print min(d)            
 
-    # x = [100000,200000,300000,400000]
+    # x = [25000,50000,100000,200000]
     # y = [2.8, 3.2, 3.6, 4.4, 4.8, 5.2]
     # z = [5.0,6.0,7.0,8.0,9.0]
-
-    from mpl_toolkits.mplot3d import Axes3D
-    from matplotlib import cm
-    from matplotlib.ticker import LinearLocator, FormatStrFormatter
-    import matplotlib.pyplot as plt
-    import numpy as np
 
     fig = plt.figure(figsize=(10,10),dpi=100)
     ax = fig.gca(projection='3d')
@@ -96,7 +108,6 @@ for n in [25000,50000,100000,200000]:
 
     # NORMED
 
-
     fig = plt.figure(figsize=(10,10),dpi=100)
     ax = fig.gca(projection='3d')
     Y, Z = np.meshgrid(z, y)
@@ -110,17 +121,36 @@ for n in [25000,50000,100000,200000]:
                            linewidth=0, antialiased=True)
     ax.set_zlim(1, 16)
 
-    ax.zaxis.set_major_locator(LinearLocator(6))
+    ax.xaxis.set_major_locator(LinearLocator(5))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+    ax.yaxis.set_major_locator(LinearLocator(4))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+    ax.zaxis.set_major_locator(LinearLocator(4))
     ax.zaxis.set_major_formatter(FormatStrFormatter('%.0f'))
 
     # fig.colorbar(surf, shrink=0.5, aspect=5)
 
-    ax.set_xlabel('$k_{lim}$',fontsize=25)
-    ax.set_ylabel('$k_{v,min}$',fontsize=25)
-    ax.set_zlabel('$T/T_{min}$',fontsize=25)
+    ax.set_xlabel('$k_{lim}\ \mathrm{[ped/m^2]}$', fontsize=32)
+    ax.set_ylabel('$k_{v,min}\ \mathrm{[ped/m^2]}$', fontsize=32)
+    ax.set_zlabel('$T/T_{min}$', fontsize=32)
 
-    ax.tick_params(axis='both', which='major', labelsize=15)
-    ax.tick_params(axis='both', which='minor', labelsize=15)
+    ax.xaxis._axinfo['label']['space_factor'] = 2.5
+    ax.yaxis._axinfo['label']['space_factor'] = 2.5
+    ax.zaxis._axinfo['label']['space_factor'] = 2.5
+
+    ax.tick_params(axis='both', labelsize=20)
 
     plt.savefig('figs/{}-norm.pdf'.format(n))
     plt.show()
+
+# ---------------
+# Draw the fundamental diagrams
+# Figures appear in figs/ folder
+# May need to switch off the legend in abm.py file for TGF paper
+
+for k_vmin in [4.4, 5.3]:
+    for k_lim in [5.0, 9.0]:
+        fd = abm.FundamentalDiagram(speedup=60,k_vmin=k_vmin,k_lim=k_lim)
+        fd.figure()
